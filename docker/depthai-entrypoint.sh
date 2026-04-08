32d6a4629cb73abd0e6e5aafa0c78d0a9ec08238
SarangaR <sarnga@gmail.com>
2026-04-08 00:38:10 -0400

    arch based ros command

--- docker/depthai-entrypoint.sh
+++ docker/depthai-entrypoint.sh
@@ -4,11 +4,17 @@ set -e
 source /ws/install/setup.bash
 
 echo "Launching Driver Module"
-ros2 launch depthai_ros_driver_v3 driver.launch.py rs_compat:=true &
+if [[ "$SW9_ARCH" == "kilted-latest" ]]; then
+
+if [[ "$SW9_ARCH" == "kilted-arm64-latest" ]]; then
+    echo "Running on ARM architecture."
+    ros2 launch depthai_ros_driver driver.launch.py rs_compat:=true &
+elif [[ "$SW9_ARCH" == "kilted-latest" ]]; then
+    echo "Running on x86 architecture."
+    ros2 launch depthai_ros_driver_v3 driver.launch.py rs_compat:=true &
+else
+    echo "SW9_ARCH is unknown or not set. Current value: $SW9_ARCH"
+fi
 
 DEPTHAI_PID=$!
-
-echo "Waiting for DepthAI topics..."
-sleep 5
-
 wait $DEPTHAI_PID
